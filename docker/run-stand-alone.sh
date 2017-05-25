de29fc057ad92b4dd0af06f20ea64e0550d29168
Garrett Graber <garrett_graber@yahoo.com>
2017-05-25 01:13:17 -0600

    Updated run-stand-alone.sh to map container port to port 80

--- docker/run-stand-alone.sh
+++ docker/run-stand-alone.sh
@@ -6,5 +6,5 @@
 docker rm -f galaxy-map-react-leaflet
 echo "Running galaxy-map-react-leaflet..."
 
-docker run --name galaxy-map-react-leaflet  -v /${PWD}/../://root/app  -p 8108:8108 galaxy-map-react-leaflet
+docker run --name galaxy-map-react-leaflet  -v /${PWD}/../://root/app  -p 80:8108 galaxy-map-react-leaflet
 
