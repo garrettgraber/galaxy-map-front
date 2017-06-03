c8f36928d18a05e94ffbd17c0ab58b9bdb642300
Garrett Graber <garrett_graber@yahoo.com>
2017-06-03 06:22:07 -0600

    Added res.redirect

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if(DatabaseLinks.hasOwnProperty('api') && isDeveloping) {
 } else if(isProduction) {
 
   // var API = 'ec2-52-205-105-107.compute-1.amazonaws.com';
-  var API = '172.31.78.38:80'
+  var API = 'http://172.31.78.38:80'
   var hostname = '0.0.0.0';
 
 
@@ -95,7 +95,7 @@ console.log("ip: ", ip.address());
     console.log("request path: ", req.path);
     // req.session.valid = true;
 
-    // res.redirect(API + req.path);
+    res.redirect(API + req.path);
   });
 
 // } else {
