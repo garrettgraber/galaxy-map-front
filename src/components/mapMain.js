69940ec68f55da976c9bc302db33dadaf6721f54
Garrett Graber <garrett_graber@yahoo.com>
2017-06-04 20:40:09 -0600

    Switch to S3 for tiles.

--- src/components/mapMain.js
+++ src/components/mapMain.js
@@ -163,7 +163,7 @@ class MapMain extends React.Component {
 
     					<Pane name="galaxy-pane" style={{ zIndex: zIndexGalaxy }}>
 
-							<TileLayer url={tileServerUrl} tms={true} crs={L.CRS.Simple} maxBoundsViscosity={1.0} minZoom={minZoom} maxZoom={maxZoom}/>
+							<TileLayer url={awsTileServerUrl} tms={true} crs={L.CRS.Simple} maxBoundsViscosity={1.0} minZoom={minZoom} maxZoom={maxZoom}/>
 
 						</Pane>
 
