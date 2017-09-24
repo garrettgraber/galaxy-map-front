78534f98f34b127f6b2b80938535c120a7e998b6
garrettgraber <garrett_graber@yahoo.com>
2017-09-24 13:01:14 -0600

    Pre Webpack 3 upgrade commit, God Speed

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,13 @@
-
-
-
-// var path = require('path');
-// var webpack = require('webpack');
-
-
-// module.exports = {
-//   context: __dirname + "/",
-//   entry: {
-//     javascript: './scripts/index.js',
-//     html: './index.html'
-//   },
-//   output: {
-//     path: __dirname + "/dist",
-//     filename: "bundle.js"
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?$/,
-//         loader: 'babel-loader',
-//         exclude: /node_modules/
-//       }
-//     ]
-//   }
-// }
-
-
-
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 console.log("__dirname: ", __dirname);
 
-
-
-
-var javascriptEntryPath = path.resolve(__dirname, 'src', 'index.js');
-var htmlEntryPath = path.resolve(__dirname, 'src', 'index.html');
-var buildPath = path.resolve(__dirname, 'public', 'build');
-var srcPath = path.resolve(__dirname, 'src');
+const javascriptEntryPath = path.resolve(__dirname, 'src', 'index.js');
+const htmlEntryPath = path.resolve(__dirname, 'src', 'index.html');
+const buildPath = path.resolve(__dirname, 'public', 'build');
+const srcPath = path.resolve(__dirname, 'src');
 
 
 console.log("javascriptEntryPath: ", javascriptEntryPath);
@@ -84,47 +50,18 @@ module.exports = {
                 test: /\.html$/,
                 loader: 'file?name=[name].[ext]',
             },
-            // {
-            //   //IMAGE LOADER
-            //   test: /\.(jpe?g|png|gif|svg)$/i,
-            //   loader:'file'
-            // },
-            // {
-            //   test: /\.css$/,
-            //   loader: 'style-loader'
-            // },
             {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader'
             },
             {   
-                test: /\.(png|jpg)$/,
+                test: /\.(png|jpg|gif)$/,
                 loader: "file-loader?name=images/[name].[ext]"
             },
             {   
                 test: /\.(svg|ico)$/,
                 loader: "file-loader?name=[path][name].[ext]"
             },
-            // {
-            //     test: /\.css$/,
-            //     loader: 'css-loader',
-            //     query: {
-            //         // modules: true,
-            //         localIdentName: '[name]__[local]___[hash:base64:5]'
-            //     }
-            // },
-            // {
-            //     test: /\.(jpg|png)$/,
-            //     // loader: 'file-loader',
-            //     loader: 'file-loader?name=[path][name].[ext]',
-            //     options: {
-            //         name: '../map-tiles/[path][name].[ext]',
-            //     }
-            // },
-            // {
-            //     test: /\.(eot|ttf|woff|woff2)$/,
-            //     loader: 'font-loader?name=[path][name].[ext]'
-            // },
             {
                 test: /\.(woff|eot|ttf|woff2)(\?.*$|$)/,
                 loader: 'url-loader?limit=100000&mimetype=application/font-woff'
@@ -133,53 +70,11 @@ module.exports = {
                 test: /\.json$/,
                 loader: "json-loader"
             }
-        ], 
+        ],
     },
     plugins: [ 
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
     ]
-}
-
-
-
-// module.exports = {
-//     target: "web",
-//     cache: true,
-//     entry: {
-//         app: path.join(srcPath, "index.js")
-//     },
-//     output: {
-//         path: path.join(__dirname, "dist"),
-//         publicPath: "/dist/",
-//         filename: 'bundle.js',
-//         pathInfo: true
-//     },
-//     resolve: {
-//         extensions: ['', '.html', '.js', '.json', '.scss', '.css'],
-//         alias: {
-//             leaflet_css: __dirname + "/node_modules/leaflet/dist/leaflet.css",
-//             leaflet_marker: __dirname + "/node_modules/leaflet/dist/images/marker-icon.png",
-//             leaflet_marker_2x: __dirname + "/node_modules/leaflet/dist/images/marker-icon-2x.png",
-//             leaflet_marker_shadow: __dirname + "/node_modules/leaflet/dist/images/marker-shadow.png"
-//         }
-//     },
-//     module: {
-//         loaders: [
-//           {test: /\.js?$/, exclude: /node_modules/, loader: "babel-loader"},
-//           {test: /\.scss?$/, exclude: /node_modules/, loader: "style-loader!css-loader!sass-loader!"},
-//           {test: /\.css?$/, loader: "style-loader!css-loader!"},
-//           {test: /\.(png|jpg)$/, loader: "file-loader?name=images/[name].[ext]"}
-//         ]
-//     },
-//     plugins: [
-//         new webpack.optimize.CommonsChunkPlugin("common", "common.js"),
-//         new HtmlWebpackPlugin({
-//           inject: true,
-//           template: "src/index.html"
-//         }),
-//         new webpack.NoErrorsPlugin()
-//     ]
-
-// }
+};
