c837c0bc08b70c31aa5368c5934c7320dae83811
garrettgraber <garrett_graber@yahoo.com>
2017-10-10 20:59:02 -0600

    add shell script to run as a daemon

--- docker/run-production.sh
+++ docker/run-production.sh
@@ -6,5 +6,5 @@
 docker rm -f galaxy-map-react-leaflet 
 echo "Running galaxy-map-react-leaflet..."
 
-docker run --name galaxy-map-react-leaflet  -d  --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8108 galaxy-map-react-leaflet
+docker run --name galaxy-map-react-leaflet   --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8108 galaxy-map-react-leaflet
 
