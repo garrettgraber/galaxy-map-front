651f04aeeac1ba0154cae9bbe88099f12c93ab5b
blade-runner <garrett_graber@yahoo.com>
2018-02-12 02:22:23 -0700

    Added .js test for uglify.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
 	plugins: [
-		new UglifyJSPlugin()
+		new UglifyJSPlugin({
+	    test: /\.js($|\?)/i
+	  })
 	]
 });
