ff443ba2cbbffa07f0df9625434deebd54ab1c12
blade-runner <garrett_graber@yahoo.com>
2018-05-15 15:58:46 -0600

    Changed compile command to npm build.

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -36,7 +36,7 @@ echo "Run status in bootstrap: $RUN_STATUS"
 
 if [ "$RUN_STATUS" == "compile_production" ]; then
   echo "Compiling webpack for production...'"
-  npm compile:production 
+  npm build 
 fi
 
 if [ "$RUN_STATUS" != "compile_production" ]; then
