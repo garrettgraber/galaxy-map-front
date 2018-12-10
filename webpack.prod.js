2a93c3942f2b370e7e9fe610ab7f6fd99f23c417
blade-runner <garrett_graber@yahoo.com>
2018-12-09 21:43:42 -0700

    Removed UglifyJSPlugin for the moment.

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,37 +10,37 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    new UglifyJSPlugin({
-      test: /[\/.](js)$/i,
-      sourceMap: false,
-      parallel: true,
-      exclude: [/\/images/, /[\/.](json)$/i],
-      uglifyOptions: {
-        mangle: false,
-        compress: {
-          warnings: false, // Suppress uglification warnings
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-          ie8: false,
-          conditionals: true,
-          unused: true,
-          comparisons: true,
-          sequences: true,
-          dead_code: true,
-          evaluate: true,
-          if_return: true,
-          join_vars: true,
-          drop_debugger: true,
-          drop_console: true, // strips console statements
-          booleans: true,
-        },
-        output: {
-          comments: false,
-        }
-      },
-      cache: true
-    }),
+    // new UglifyJSPlugin({
+    //   test: /[\/.](js)$/i,
+    //   sourceMap: false,
+    //   parallel: true,
+    //   exclude: [/\/images/, /[\/.](json)$/i],
+    //   uglifyOptions: {
+    //     mangle: false,
+    //     compress: {
+    //       warnings: false, // Suppress uglification warnings
+    //       pure_getters: true,
+    //       unsafe: true,
+    //       unsafe_comps: true,
+    //       ie8: false,
+    //       conditionals: true,
+    //       unused: true,
+    //       comparisons: true,
+    //       sequences: true,
+    //       dead_code: true,
+    //       evaluate: true,
+    //       if_return: true,
+    //       join_vars: true,
+    //       drop_debugger: true,
+    //       drop_console: true, // strips console statements
+    //       booleans: true,
+    //     },
+    //     output: {
+    //       comments: false,
+    //     }
+    //   },
+    //   cache: true
+    // }),
     new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
     new CompressionPlugin({
       asset: "[path].gz[query]",
