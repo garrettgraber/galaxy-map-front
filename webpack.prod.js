a2a4e05caf9c38657f90e65b446a6d1b6dc603bc
blade-runner <garrett_graber@yahoo.com>
2018-12-12 20:58:58 -0700

    Added UglifyJs plugin back in. It's working! It's working!

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,46 +1,51 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const CompressionPlugin = require('compression-webpack-plugin');
 const common = require('./webpack.config.js');
 
 
 module.exports = merge(common, {
   plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    // new UglifyJSPlugin({
-    //   test: /[\/.](js)$/i,
-    //   sourceMap: false,
-    //   parallel: true,
-    //   exclude: [/\/images/, /[\/.](json)$/i],
-    //   uglifyOptions: {
-    //     mangle: false,
-    //     compress: {
-    //       warnings: false, // Suppress uglification warnings
-    //       pure_getters: true,
-    //       unsafe: true,
-    //       unsafe_comps: true,
-    //       ie8: false,
-    //       conditionals: true,
-    //       unused: true,
-    //       comparisons: true,
-    //       sequences: true,
-    //       dead_code: true,
-    //       evaluate: true,
-    //       if_return: true,
-    //       join_vars: true,
-    //       drop_debugger: true,
-    //       drop_console: true, // strips console statements
-    //       booleans: true,
-    //     },
-    //     output: {
-    //       comments: false,
-    //     }
-    //   },
-    //   cache: true
-    // }),
+    new UglifyJSPlugin({
+      test: /[\/.](js)$/i,
+      sourceMap: false,
+      parallel: true,
+      exclude: [/\/images/, /[\/.](json)$/i],
+      uglifyOptions: {
+        warnings: false,
+        parse: {},
+        compress: {
+          warnings: false, // Suppress uglification warnings
+          pure_getters: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+          drop_debugger: true,
+          drop_console: true, // strips console statements
+          booleans: true
+        },
+        mangle: false, // Note `mangle.properties` is `false` by default.
+        output: {
+          comments: false
+        },
+        toplevel: false,
+        nameCache: null,
+        ie8: false,
+        keep_fnames: false,
+      },
+      cache: true
+    }),
     new webpack.optimize.AggressiveMergingPlugin(), //Merge chunks
     new CompressionPlugin({
       asset: "[path].gz[query]",
