6f4096a0563958f030911885a43c43c1bfdd17e9
blade-runner <garrett_graber@yahoo.com>
2019-03-14 14:36:25 -0600

    Production containers are now setup to automatically restart.

--- docker/run-production-daemon-https.sh
+++ docker/run-production-daemon-https.sh
@@ -7,4 +7,4 @@ docker rm -f galaxy-map-react-leaflet
 echo "Running galaxy-map-react-leaflet..."
 
 
-docker run --name galaxy-map-react-leaflet  -d  --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 8108:8108 galaxy-map-react-leaflet
+docker run --restart=always --name galaxy-map-react-leaflet  -d  --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 8108:8108 galaxy-map-react-leaflet
