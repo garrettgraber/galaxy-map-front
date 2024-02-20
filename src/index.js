1f311b4de1ca36ad4a65d5a2a221b81f92c311f0
garrettgraber <garrett_graber@yahoo.com>
2024-02-20 13:24:56 -0700

    Added react-tooltip css import in /src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import 'react-tooltip/dist/react-tooltip.css';
+
 import App from './components/app.js';
 import store from './stores/store.js';
 
